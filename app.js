b56b024aa8247fb7c362cb5e1b046df610de5ab6
512896 <kmaheshwariit@gmail.com>
2016-07-18 13:08:06 +0530

    Added app.js,Package.json

---
+++ app.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+//lets require/import the mongodb native drivers.
+var mongodb = require('mongodb');
+
+//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+var MongoClient = mongodb.MongoClient;
+
+// Connection URL. This is where your mongodb server is running.
+var url = 'mongodb://localhost:27017/company';
+
+// Use connect method to connect to the Server
+MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    //HURRAY!! We are connected. :)
+    console.log('Connection established to', url);
+
+    // Get the documents collection
+    var collection = db.collection('info');
+
+    //Create some users
+    //"NAME" : "Anitha", "Emp_ID" : "512896", "Project" : "Mickensy" 
+    var user1 = {NAME: 'Maheshwari', Emp_ID: "42", Project: 'adminProject'};
+    var user2 = {NAME: 'modulus user', Emp_ID: "22", Project: 'userProject'};
+    var user3 = {NAME: 'modulus super admin', Emp_ID: "92", Project: 'super-admin'};
+
+    // Insert some users
+    collection.insert([user1, user2, user3], function (err, result) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Inserted %d documents into the "users" collection. The documents inserted with "_id" are:', result.length, result);
+      }
+      //Close connection
+      db.close();
+    });
+  }
+});
